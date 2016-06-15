26937393c8c96cec1d70aa7ce06f97f979564561
baronhq <26222521@qq.com>
2016-06-15 14:30:37 +0800

    第一章

---
+++ ASP.NET WEB API 2框架揭秘/Chapter 01/S101/WebApp/Scripts/viewmodeldistrict.js
@@ -0,0 +1,21 @@
+﻿function AddressModel() {
+    var self = this;
+    self.province = ko.observable("江苏省");
+    self.city = ko.observable("苏州市");
+    self.district = ko.observable("工业园区");
+    self.street = ko.observable("星湖街328号");
+    self.address = ko.observable();
+
+    self.format = function () {
+        if (self.province() && self.city() && self.district() && self.street()) {
+            var address = self.province() + ' ' + self.city() + ' ' + self.district() + ' ' + self.street();
+            self.address(address);
+        } else {
+            alert('请提供完整信息。');
+        }
+    };
+
+    self.format();
+}
+
+ko.applyBindings(new AddressModel());
